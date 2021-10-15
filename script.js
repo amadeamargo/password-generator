4bb60f7886054a0565317fe66ff66f247deb0f77
Amadea Margo <amadeamargo2022@u.northwestern.edu:git config user.emailgit config --global user.name Amadea>
2021-10-15 02:44:59 -0500

    getRandom functions created

--- script.js
+++ script.js
@@ -7,24 +7,48 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 
-  var Passwordlength = window.prompt("How many characters would you like in your password: ");
-  var MaxLength = window.prompt("Maximum length of password (in characters)");
-  var LowerCase = "Would you like to include lower case characters?"
-  var UpperCase = "Would you like to include upper case characters?"
-  var Numeric = "Would you like to include numeric characters?"
-  var SpecialCharacters = "Would you like to include special characters?"
+  var passwordLengthPrompt = window.prompt("How many characters would you like in your password: ");
+  var maxLengthPrompt = window.prompt("Maximum length of password (in characters)");
+  var lowerCaseConfirm = "Would you like to include lower case characters?"
+  var upperCaseConfirm = "Would you like to include upper case characters?"
+  var numericConfirm = "Would you like to include numeric characters?"
+  var specialCharactersConfirm = "Would you like to include special characters?"
 
+  console.log (passwordLengthPrompt);
+  console.log (maxLengthPrompt);
 
-  console.log (Passwordlength);
-  console.log (MaxLength);
+  confirm(lowerCaseConfirm);
+  confirm(upperCaseConfirm);
+  confirm(numericConfirm);
+  confirm(specialCharactersConfirm);
 
+  function getRandomLower (){
+      var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+      var randomLower = lowerCase [Math.floor(Math.random() * 26)];
+      console.log (randomLower);
+  }
 
-  confirm(LowerCase);
-  confirm(UpperCase);
-  confirm(Numeric);
-  confirm(SpecialCharacters);
+  function getRandomUpper(){
+      const upperCase = "ABCDEFGHJIJKLMNOPQRSTUVWXYZ";
+      var randomUpper = upperCase [Math.floor(Math.random() * 26)];
+      console.log (randomUpper);
+  }
 
- 
+  function getRandomNumeric (){
+      const numeric = "12345678790";
+      var randomNumeric = numeric [Math.floor(Math.random() * 26)];
+      console.log (randomNumeric);
+  }
+
+  function getRandomSpecialChar (){
+      const specialChar = "!#$%^&'()*+,-./:;<=>?@[\]^_`{|}~";
+      var randomSpecialChar = specialChar [Math.floor(Math.random() * 32)];
+      console.log (randomSpecialChar);
+  }
+  getRandomLower();
+  getRandomUpper();
+  getRandomNumeric();
+  getRandomSpecialChar();
 }
 
 // Add event listener to generate button
@@ -37,5 +61,6 @@ generateBtn.addEventListener("click", writePassword);
     //USER INPUT length of characters: at least 8 characters
     //USER INPUT max characters: max 128 characters
 //CONFIRM: include lcase, upcase, num, or spec char
+
 //output: at least one of the characters appear in the password
 //ALERT: display password 
